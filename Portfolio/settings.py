b104fd50ef56bcae4fad2daa28b4b5fa3d6a12e0
dewanisonal03@gmail.com <dewanisonal03@gmail.com>
2026-03-29 20:37:14 +0530

    Added .gitignore and removed sensitive files

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +29,7 @@ SECRET_KEY = 'django-insecure-244_*3+d5k4y$-sv+lxpb4=9#xesp-x7erga#z25_kxrg1a-b$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -113,11 +118,14 @@ USE_TZ = True
 
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'dewanisonal03@gmail.com'
-EMAIL_HOST_PASSWORD = 'pkxr yixs cdzl xhju'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+BASE_DIR = Path(__file__).resolve().parent.parent
